38c279cdc749539c50e2499791c412c00347feba
Marko Kreen <markokr@gmail.com>
2013-11-15 01:11:05 +0200

    SSL: better default ciphersuite
    https://commitfest.postgresql.org/action/patch_view?id=1310
    http://archives.postgresql.org/message-id/20131114231105.GA23669@gmail.com

--- src/backend/utils/misc/guc.c
+++ src/backend/utils/misc/guc.c
@@ -3133,7 +3133,7 @@ static struct config_string ConfigureNamesString[] =
 		},
 		&SSLCipherSuites,
 #ifdef USE_SSL
-		"DEFAULT:!LOW:!EXP:!MD5:@STRENGTH",
+		"HIGH:!aNULL",
 #else
 		"none",
 #endif
