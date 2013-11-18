0c06d753db27df821af7925e508df551f460302d
KONDO Mitsumasa <kondo.mitsumasa@lab.ntt.co.jp>
2013-11-18 20:36:43 +0900

    Improvement of pg_stat_statement usage about buffer hit ratio
    https://commitfest.postgresql.org/action/patch_view?id=1260
    http://archives.postgresql.org/message-id/5289FBCB.5000601@lab.ntt.co.jp

--- contrib/pg_stat_statements/pg_stat_statements--1.2.sql
+++ contrib/pg_stat_statements/pg_stat_statements--1.2.sql
@@ -1,4 +1,4 @@
-/* contrib/pg_stat_statements/pg_stat_statements--1.1.sql */
+/* contrib/pg_stat_statements/pg_stat_statements--1.2.sql */
 
 -- complain if script is sourced in psql, rather than via CREATE EXTENSION
 \echo Use "CREATE EXTENSION pg_stat_statements" to load this file. \quit
@@ -35,7 +35,29 @@ LANGUAGE C;
 
 -- Register a view on the function for ease of use.
 CREATE VIEW pg_stat_statements AS
-  SELECT * FROM pg_stat_statements();
+  SELECT userid,
+         dbid,
+         query,
+         calls,
+         total_time,
+         total_time / calls::float AS avg_time,
+         rows,
+         CASE WHEN shared_blks_hit + shared_blks_read > 0
+           THEN 100.0 * (shared_blks_hit::float / (shared_blks_hit + shared_blks_read))
+           ELSE 0 END AS shared_blks_hit_percent,
+         shared_blks_hit,
+         shared_blks_read,
+         shared_blks_dirtied,
+         shared_blks_written,
+         local_blks_hit,
+         local_blks_read,
+         local_blks_dirtied,
+         local_blks_written,
+         temp_blks_read,
+         temp_blks_written,
+         blk_read_time,
+         blk_write_time
+  FROM pg_stat_statements();
 
 GRANT SELECT ON pg_stat_statements TO PUBLIC;
 
